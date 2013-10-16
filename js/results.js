d8df3863599dd0e69f5fea46e3b0c0dc8ae4a4e8
Anne Rose <rose@cs.umd.edu>
2013-10-16 16:04:09 -0400

    Turn off "show note in single category" by default

--- js/results.js
+++ js/results.js
@@ -31,7 +31,7 @@ var showSubcategories = false;
 var hasSubcategories = false;
 
 var DISPLAY_ITEM_IN_SINGLE_CATEGORY = true;
-var singleCategoryOnly = true;
+var singleCategoryOnly = false;
 var showOnlyInCategories = {};
 
 var showExpanded = true;
